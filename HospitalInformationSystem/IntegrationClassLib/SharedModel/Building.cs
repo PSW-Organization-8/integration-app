c86bce29c7d145b6e276f4e61afbe8575b3fedf6
PuricJelena <86349060+PuricJelena@users.noreply.github.com>
2021-11-22 11:50:05 +0100

    FEAT: model of hospital

---
+++ HospitalInformationSystem/IntegrationClassLib/SharedModel/Building.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationClassLib.SharedModel
+{
+    public class Building
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+
+        public Building()
+        {
+           
+        }
+
+        public Building(string id, string name)
+        {
+            this.ID = id;
+            this.Name = name;
+               
+        }
+
+    }
+}
