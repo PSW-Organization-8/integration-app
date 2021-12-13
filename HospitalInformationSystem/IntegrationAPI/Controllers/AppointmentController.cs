b4235a13df1340931b9ab03bb896f4b3170c3e42
Violeta Kukic <vkukic99@gmail.com>
2021-12-13 19:33:19 +0100

    feat: Sprint 3-Renavate room(next/back)

---
+++ HospitalInformationSystem/IntegrationAPI/Controllers/AppointmentController.cs
@@ -0,0 +1,35 @@
+﻿using IntegrationClassLib.Equipment.Service;
+using IntegrationClassLib.SharedModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegrationAPI.Controllers
+{
+    public class AppointmentController
+    {
+
+        private readonly AppointmentService appointmentService;
+        public AppointmentController(AppointmentService appointmentService)
+        {
+            this.appointmentService = appointmentService;
+        }
+
+        [HttpGet]
+        public List<Appointment> GetAllAppointment()
+        {
+            return appointmentService.GetAllAppointments();
+        }
+
+
+
+        [HttpPost]
+        [Route("appointment")]
+        public Appointment CreateAppointment(Appointment appointment)
+        {
+            return appointmentService.CreatAppointment(appointment);
+        }
+    }
+}
---
+++ HospitalInformationSystem/IntegrationAPI/Migrations/20211213084948_Appointment.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace IntegrationAPI.Migrations
+{
+    public partial class Appointment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DateandTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    RoomID = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointments", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Rooms_RoomID",
+                        column: x => x.RoomID,
+                        principalTable: "Rooms",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_RoomID",
+                table: "Appointments",
+                column: "RoomID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointments");
+        }
+    }
+}
