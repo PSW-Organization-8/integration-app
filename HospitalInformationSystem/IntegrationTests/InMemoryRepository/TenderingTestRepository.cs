fc625b499b61dd5d03f1c2f66c45622cdd075140
Mihajlo Mitić <mihajlomiticmiki@gmail.com>
2021-12-28 18:34:52 +0100

    test: added tendering tests

---
+++ HospitalInformationSystem/IntegrationTests/InMemoryRepository/TenderingTestRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IntegrationClassLib.Tendering.Model;
+using IntegrationClassLib.Tendering.Repository;
+
+namespace IntegrationTests.InMemoryRepository
+{
+    public class TenderingTestRepository : ITenderingRepository
+    {
+        private Dictionary<long, Tender> allTender = new();
+
+        public TenderingTestRepository()
+        {
+            allTender.Add(1, new Tender(){EndDate = null, Id = 1, Name = "tender1", TenderMedications = new List<TenderMedication>{ new TenderMedication() { Id = 1, TenderId = 1, Quantity = 1, MedicationName = "asd" } } });
+        }
+
+        public List<Tender> GetAll()
+        {
+            return allTender.Values.ToList();
+        }
+
+        public Tender Get(long id)
+        {
+            return allTender.ContainsKey(id) ? allTender[id] : null;
+        }
+
+        public Tender Update(Tender t)
+        {
+            allTender[t.Id] = t;
+            return allTender[t.Id];
+        }
+
+        public Tender Create(Tender t)
+        {
+            allTender.Add(t.Id, t);
+            return allTender[t.Id];
+        }
+
+        public bool ExistsById(long id)
+        {
+            return allTender.ContainsKey(id);
+        }
+
+        public bool Delete(long id)
+        {
+            return allTender.Remove(id);
+        }
+
+        public List<Tender> GetAllWithMedications()
+        {
+            return allTender.Values.ToList();
+        }
+    }
+}
---
+++ HospitalInformationSystem/IntegrationTests/IntegrationTests/TenderingTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IntegrationAPI.Connection;
+using IntegrationAPI.Controllers;
+using IntegrationAPI.Dto;
+using IntegrationClassLib;
+using IntegrationClassLib.Pharmacy.Repository.PharmacyRepo;
+using IntegrationClassLib.Pharmacy.Service;
+using IntegrationClassLib.Tendering.Repository;
+using IntegrationClassLib.Tendering.Service;
+using IntegrationClassLib.Tendering.Service.Interface;
+using IntegrationTests.InMemoryRepository;
+using Microsoft.AspNetCore.Mvc;
+using Shouldly;
+using Xunit;
+
+namespace IntegrationTests.IntegrationTests
+{
+    public class TenderingTests
+    {
+        private ITenderingRepository tenderingRepository;
+
+        [Theory]
+        [MemberData(nameof(CreatingTenderTestData))]
+        public void creating_tenders(string tenderName, string complationDate, List<TenderMedicationDto> medicationDtos,
+            ObjectResult objectResult, int tenderNum)
+        {
+            TenderingController tenderingController = GetTenderingController();
+
+            IActionResult retVal = tenderingController.CreateTender(new TenderDto()
+                { tenderName = tenderName, complationDate = complationDate, medications = medicationDtos });
+
+            retVal.ShouldBeOfType(objectResult.GetType());
+            tenderingController.GetAll().Count.ShouldBe(tenderNum);
+        }
+
+        [Theory]
+        [MemberData(nameof(ClosingTenderTestData))]
+        public void closing_tenders(long id, StatusCodeResult objectResult)
+        {
+            TenderingController tenderingController = GetTenderingController();
+
+            IActionResult retVal = tenderingController.CloseTender(id);
+
+            retVal.ShouldBeOfType(objectResult.GetType());
+        }
+
+        [Fact]
+        public void tenders_number()
+        {
+            TenderingController tenderingController = GetTenderingController();
+
+            var retVal = tenderingController.GetAll();
+
+            retVal.Count.ShouldBe(1);
+        }
+
+        private TenderingController GetTenderingController()
+        {
+            MyDbContext dbContext = new MyDbContext();
+            tenderingRepository = new TenderingTestRepository();
+            TenderCommunicationRabbitMQService tenderCommunicationRabbitMqService =
+                new TenderCommunicationRabbitMQService();
+            ITenderService tenderService = new TenderService(tenderingRepository, tenderCommunicationRabbitMqService);
+            IPharmacyOfferRepository offerRepository = new PharmacyOfferRepository(dbContext);
+            IPharmacyOfferService pharmacyOfferService =
+                new PharmacyOfferService(offerRepository, tenderService, tenderCommunicationRabbitMqService);
+            TenderingController controller =
+                new TenderingController(tenderService, pharmacyOfferService, new HospitalHttpConnection());
+            return controller;
+        }
+
+        public static IEnumerable<object[]> CreatingTenderTestData()
+        {
+            var retVal = new List<object[]>();
+
+            retVal.Add(new object[]
+                { "tenderTest1", "badDate", null, new BadRequestObjectResult(""), 1 });
+            retVal.Add(new object[]
+                { "tenderTest2", "2020-08-08", null, new BadRequestObjectResult(""), 1 });
+            retVal.Add(new object[]
+                { "tenderTest2", "2022-08-08", null, new BadRequestObjectResult(""), 1 });
+            /*retVal.Add(new object[]
+            {
+                "tenderTest2", "2022-08-08",
+                new List<TenderMedicationDto> { new() { quantity = 1, medicationName = "asd" } },
+                new OkObjectResult(""), 2
+            });*/
+
+            return retVal;
+        }
+
+        public static IEnumerable<object[]> ClosingTenderTestData()
+        {
+            var retVal = new List<object[]>();
+
+            retVal.Add(new object[] { -1, new BadRequestResult() });
+            retVal.Add(new object[] { 1, new OkResult() });
+            return retVal;
+        }
+    }
+}
