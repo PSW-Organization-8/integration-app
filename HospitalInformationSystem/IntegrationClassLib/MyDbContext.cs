10dcd936512136ef67a0771ac93e8f1b469b43a0
Mihajlo Mitić <mihajlomiticmiki@gmail.com>
2022-01-11 14:58:31 +0100

    feat: sending email when tender is finished
    fix: added environment variables for email

--- HospitalInformationSystem/IntegrationClassLib/MyDbContext.cs
+++ HospitalInformationSystem/IntegrationClassLib/MyDbContext.cs
@@ -12,7 +12,6 @@ namespace IntegrationClassLib
 {
     public class MyDbContext : DbContext
     {
-
         public DbSet<Pharmacy.Model.Pharmacy> Pharmacies { get; set; }
         public DbSet<News> News { get; set; }
 
@@ -34,10 +33,11 @@ namespace IntegrationClassLib
 
         public MyDbContext()
         {
-
         }
 
-        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -48,38 +48,72 @@ namespace IntegrationClassLib
             String password = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "root";
 
 
-            String connectionString = $"Server={server}; Port ={port}; Database ={databaseName}; User Id = {username}; Password ={password};";
+            String connectionString =
+                $"Server={server}; Port ={port}; Database ={databaseName}; User Id = {username}; Password ={password};";
             optionsBuilder.UseNpgsql(connectionString);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new NewsConfiguration());
-            
 
 
             modelBuilder.Entity<Pharmacy.Model.Pharmacy>().HasData(
-                new Pharmacy.Model.Pharmacy { Id = 1, Name = "Apoteka1", ApiKey = "fds15d4fs6", Url = "http://localhost", Port = "18013", Sftp = false },
-                new Pharmacy.Model.Pharmacy { Id = 2, Name = "Apoteka2", ApiKey = "fds15d4fs6", Url = "localhost", Port = "4111", ComunicateWithGrpc = true, Sftp = true },
-                new Pharmacy.Model.Pharmacy { Id = 3, Name = "Apoteka3", ApiKey = "fds15d4fs6", Url = "http://localhost", Port = "18013", ComunicateWithGrpc = true, Sftp = true }
+                new Pharmacy.Model.Pharmacy
+                {
+                    Id = 1, Name = "Apoteka1", ApiKey = "fds15d4fs6", Url = "http://localhost", Port = "18013",
+                    Sftp = false, EmailAddress = "psworganisation8+Apoteka1@outlook.com"
+                },
+                new Pharmacy.Model.Pharmacy
+                {
+                    Id = 2, Name = "Apoteka2", ApiKey = "fds15d4fs6", Url = "localhost", Port = "4111",
+                    ComunicateWithGrpc = true, Sftp = true, EmailAddress = "psworganisation8+Apoteka2@outlook.com"
+                },
+                new Pharmacy.Model.Pharmacy
+                {
+                    Id = 3, Name = "Apoteka3", ApiKey = "fds15d4fs6", Url = "http://localhost", Port = "18013",
+                    ComunicateWithGrpc = true, Sftp = true, EmailAddress = "psworganisation8+Apoteka3@outlook.com"
+                }
             );
             modelBuilder.Entity<Objection>().HasData(
                 new Objection { Id = 1, PharmacyName = "Apoteka1", TextObjection = "Lose usluge" }
             );
             modelBuilder.Entity<Notification>().HasData(
-               new Notification(1, "Izvestaj", true, "Ovde ce da bude tekst nekog izvestaja", "MedicationSpecifiation.pdf")
-               );
+                new Notification(1, "Izvestaj", true, "Ovde ce da bude tekst nekog izvestaja",
+                    "MedicationSpecifiation.pdf")
+            );
             modelBuilder.Entity<Response>().HasData(
-               new Response { Id = 1, ObjectionId = "1", TextResponse = "Nije tacno" }
-           );
+                new Response { Id = 1, ObjectionId = "1", TextResponse = "Nije tacno" }
+            );
             modelBuilder.Entity<MedicationConsumption>().HasData(
-             new MedicationConsumption { MedicineID = 1, MedicineName = "Brufen", DateTime = DateTime.ParseExact("11/11/2021", "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal), Quantity = 32 },
-             new MedicationConsumption { MedicineID = 2, MedicineName = "Vitamin C", DateTime = DateTime.ParseExact("11/11/2021", "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal), Quantity = 16 },
-             new MedicationConsumption { MedicineID = 3, MedicineName = "Brufen", DateTime = DateTime.ParseExact("11/11/2021", "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal), Quantity = 56 }
-         );
+                new MedicationConsumption
+                {
+                    MedicineID = 1, MedicineName = "Brufen",
+                    DateTime = DateTime.ParseExact("11/11/2021", "dd/MM/yyyy", CultureInfo.InvariantCulture,
+                        DateTimeStyles.AssumeUniversal),
+                    Quantity = 32
+                },
+                new MedicationConsumption
+                {
+                    MedicineID = 2, MedicineName = "Vitamin C",
+                    DateTime = DateTime.ParseExact("11/11/2021", "dd/MM/yyyy", CultureInfo.InvariantCulture,
+                        DateTimeStyles.AssumeUniversal),
+                    Quantity = 16
+                },
+                new MedicationConsumption
+                {
+                    MedicineID = 3, MedicineName = "Brufen",
+                    DateTime = DateTime.ParseExact("11/11/2021", "dd/MM/yyyy", CultureInfo.InvariantCulture,
+                        DateTimeStyles.AssumeUniversal),
+                    Quantity = 56
+                }
+            );
             modelBuilder.Entity<Tender>().HasData(
-                new Tender { Id = 1, Name = "Hitno", StartDate = DateTime.Now, EndDate = DateTime.Now.AddDays(3) },
-                new Tender { Id = 2, Name = "Veoma hitno", StartDate = DateTime.Now, EndDate = DateTime.Now.AddDays(5) }
+                    new Tender { Id = 1, Name = "Hitno", StartDate = DateTime.Now, EndDate = DateTime.Now.AddDays(3), WinnerOfferId = 1, IsAceptedOffer = true},
+                    new Tender
+                    {
+                        Id = 2, Name = "Veoma hitno", StartDate = DateTime.Now, EndDate = DateTime.Now.AddDays(5)
+                    }
                 )
                 ;
             modelBuilder.Entity<Tender>().Property(tender => tender.EndDate).IsRequired(false);
@@ -89,6 +123,58 @@ namespace IntegrationClassLib
                 new TenderMedication { Id = 1, MedicationName = "brufen", Quantity = 1, TenderId = 1 },
                 new TenderMedication { Id = 2, MedicationName = "ventolin", Quantity = 1, TenderId = 1 },
                 new TenderMedication { Id = 3, MedicationName = "brufen", Quantity = 1, TenderId = 2 });
+
+            modelBuilder.Entity<PharmacyOffer>().HasData(
+                    new PharmacyOffer
+                    {
+                        Id = 1,
+                        OfferIdInPharmacy = 1,
+                        PharmacyId = 1,
+                        PharmacyName = "Apoteka1",
+                        TenderId = 1,
+                        TenderIdInHospital = 1,
+                        TimePosted = DateTime.Now
+                    },
+                    new PharmacyOffer
+                    {
+                        Id = 2,
+                        OfferIdInPharmacy = 2,
+                        PharmacyId = 2,
+                        PharmacyName = "Apoteka2",
+                        TenderId = 1,
+                        TenderIdInHospital = 1,
+                        TimePosted = DateTime.Now
+                    }
+                )
+                ;
+
+            modelBuilder.Entity<PharmacyOfferComponent>().HasData(
+                    new PharmacyOfferComponent()
+                    {
+                        Id = 1,
+                        Quantity = 123,
+                        MedicationName = "brufen",
+                        PharmacyOfferId = 1,
+                        Price = 120
+                    },
+                    new PharmacyOfferComponent()
+                    {
+                        Id = 2,
+                        Quantity = 321,
+                        MedicationName = "ventolin",
+                        PharmacyOfferId = 1,
+                        Price = 222
+                    },
+                    new PharmacyOfferComponent()
+                    {
+                        Id = 3,
+                        Quantity = 455,
+                        MedicationName = "brufen",
+                        PharmacyOfferId = 2,
+                        Price = 500
+                    }
+                )
+                ;
         }
     }
 }
